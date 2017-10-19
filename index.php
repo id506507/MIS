98a7ad8b592be1fd86bebe120030979b04e9cff1
Chan Tsz Ching <id506507@gmail.com>
2017-10-19 22:20:55 +0800

    v0.2
    版面大概完成

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+     if ( !( $database = mysqli_connect( "localhost","root","","announment" ) ) )                      
+            die( "Could not connect to database </body></html>" );
+
+    if ( !mysqli_select_db(  $database,"announment" ) )
+            die( "Could not open products database </body></html>" );
+
+    mysqli_query($database,"SET NAMES UTF8");    
+    if ( !( $result = mysqli_query($database,"SELECT * FROM list ORDER BY top DESC ,no DESC") ) ) 
+         {
+            print( "<p>Could not execute query!</p>" );
+            die( mysqli_error($database) . "</body></html>" );
+         }
+
+    @$result=mysqli_query($database,"select * from list ORDER BY top DESC ,no DESC limit $start, $number");
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,19 +36,59 @@
           font-family: 'Noto Sans TC';
         }
     </style>
+<!--table-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 <!--Import jQuery before materialize.js-->
-      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-      <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
 <!--naverbar-->
-      <nav>
-        <div class="nav-wrapper">
-          <a href="index.php" class="brand-logo"><i class="material-icons">&nbsp;cloud</i>宿舍公告系統</a>
-          <ul class="right hide-on-med-and-down">
-            <li><a href="login.php"><i class="material-icons left">assignment_ind<!--icon圖示放的地方--></i>管理者登入<!--旁邊的字--></a></li>
-          </ul>
-        </div>
-      </nav>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="index.php" class="brand-logo"><i class="material-icons">&nbsp;cloud</i>宿舍公告系統</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="login.php"><i class="material-icons left">assignment_ind<!--icon圖示放的地方--></i>管理者登入<!--旁邊的字--></a></li>
+      </ul>
+    </div>
+  </nav>
+<!--container-->
+<section>
+  <h3 style="text-align: center">歡迎來到這裡。</h3>
+  <table class="table table-hover">
+        <thead>
+          <tr>
+              <th style="width:5%">編號</th>
+              <th style="width:55%">標題</th>
+              <th style="width:10%">日期</th>
+              <th style="width:10%">發佈單位</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Alvin</td>
+            <td>Eclair</td>
+            <td>$0.87</td>
+            <td>$0.87</td>
+          </tr>
+          <tr>
+            <td>Alan</td>
+            <td>Jellybean</td>
+            <td>$3.76</td>
+            <td>$0.87</td>
+          </tr>
+          <tr>
+            <td>Jonathan</td>
+            <td>Lollipop</td>
+            <td>$7.00</td>
+            <td>$0.87</td>
+          </tr>
+
+        </tbody>
+      </table>
+</section>
 </body>
 </html>
