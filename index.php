86486adaf8bf0d44e7c6b6735a797ef8e1ef9b1a
Chan Tsz Ching <id506507@gmail.com>
2017-11-12 17:19:09 +0800

    v0.3
    adding some new things

--- index.php
+++ index.php
@@ -11,8 +11,17 @@
             print( "<p>Could not execute query!</p>" );
             die( mysqli_error($database) . "</body></html>" );
          }
+    $number=15;
+    $total=mysqli_num_rows($result);//total行數     
+    $pages=ceil($total/$number);//算頁數
+    @$p=$_GET['p'];
+    if($p == '')
+      {
+        $p=1;
+      }
+      $start=($p-1)*$number;
+    @$result=mysqli_query($database,"select * from list ORDER BY top DESC ,no DESC");
 
-    @$result=mysqli_query($database,"select * from list ORDER BY top DESC ,no DESC limit $start, $number");
     
 ?>
 
@@ -56,7 +65,7 @@
   </nav>
 <!--container-->
 <section>
-  <h3 style="text-align: center">歡迎來到這裡。</h3>
+  <h3 style="text-align: center">歡迎來到這裡。在這裡可以看到有關宿舍的公佈。</h3>
   <table class="table table-hover">
         <thead>
           <tr>
@@ -68,27 +77,37 @@
         </thead>
 
         <tbody>
-          <tr>
-            <td>Alvin</td>
-            <td>Eclair</td>
-            <td>$0.87</td>
-            <td>$0.87</td>
-          </tr>
-          <tr>
-            <td>Alan</td>
-            <td>Jellybean</td>
-            <td>$3.76</td>
-            <td>$0.87</td>
-          </tr>
-          <tr>
-            <td>Jonathan</td>
-            <td>Lollipop</td>
-            <td>$7.00</td>
-            <td>$0.87</td>
-          </tr>
-
+          
+          <?php
+                    for($i=1;$i<=mysqli_num_rows($result);$i++){
+                      $row=mysqli_fetch_row($result);
+                ?>
+                <tr>
+                    <td><?php print("$row[0]"); ?></td>
+                    <td><a href="content.php?id=<?php echo $row['0']?>"><?php print("$row[1]"); ?></a></td>
+                    <td><?php print("$row[2]"); ?></td>
+                    <td><?php print("$row[3]"); ?></td>
+                </tr>
+                <?php
+                }
+                ?>
         </tbody>
       </table>
 </section>
+<div class="search">
+        <ul class="pagination">
+        <?php
+            
+          for($i=1;$i<=$pages;$i++){
+        ?>
+            <li><a href="index.php?p=<?php echo $i?>"><?php print("$i") ?></a></li>
+        <?php
+          } 
+        ?>
+        </ul>
+    </div>
+    <div>
+    <p style="text-align: center">第<?php echo $p?>頁/共有<?php echo $pages?>頁</p>
+    </div>
 </body>
 </html>
